b4c6435cd39a540f2faebc8419d1984bbd9f986e
Asif Mehedi <asif.mehedi@live.com>
2020-09-19 23:34:12 -0400

    Change dict path

--- functions/check-spelling.js
+++ functions/check-spelling.js
@@ -1,12 +1,13 @@
 // for testing
-// http://localhost:8888/.netlify/functions/spell?word=%E0%A6%86%E0%A6%A8%E0%A7%8D%E0%A6%A6%E0%A7%8B%E0%A6%B2%E0%A6%A8
-
+// local: http://localhost:8888/.netlify/functions/check-spelling?word=%E0%A6%86%E0%A6%A8%E0%A7%8D%E0%A6%A6%E0%A7%8B%E0%A6%B2%E0%A6%A8
+// live: https://spellbangla.netlify.app/.netlify/functions/check-spelling?word=%E0%A6%86%E0%A6%A8%E0%A7%8D%E0%A6%A6%E0%A7%8B%E0%A6%B2%E0%A6%A8
 const fs = require('fs');
 const path = require('path');
 const Nodehun = require('nodehun');
 
-const affbuf = fs.readFileSync(path.join(__dirname, '/dict/bn_BD.aff'));
-const dictbuf = fs.readFileSync(path.join(__dirname, '/dict/bn_BD.dic'));
+// todo: move dict folder outside of functions folder since netlify complains when deploying
+const affbuf = fs.readFileSync(path.resolve(__dirname, '../dict/bn_BD.aff'));
+const dictbuf = fs.readFileSync(path.resolve(__dirname, '../dict/bn_BD.dic'));
 const nodehun = new Nodehun(affbuf, dictbuf);
 
 exports.handler = async (event, context, callback) => {
